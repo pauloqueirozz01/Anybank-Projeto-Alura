536e6563ac65cbaf426ad7d52445660fe8f185b8
Paulo Queiroz | IOS Dev <pauloqueirozzdev@gmail.com>
2026-06-08 19:19:02 -0300

    criando metodo construtor da class Conta e chamando ela dentro do programa, acompanhe o script em loopsPoo/contas.dart

--- bin/loopsPoo/contas.dart
+++ bin/loopsPoo/contas.dart
@@ -1,5 +1,29 @@
 import 'dart:io';
 
+class Conta {
+  String nome;
+  double saldo;
+
+  Conta(this.nome, this.saldo);
+
+  // Estamos criando MÉTODOS ou MÉTODO CONSTRUTOR, que nada mais são do quê funções dentro de Classes.
+  void receiving(double value) {
+    // Podemos chamar diretamente o saldo para atribuir o novo value
+    saldo += value;
+    //esse += é a mesma coisa de escrever conta.saldo = conta.saldo + value;
+    print("$nome recebeu uma transferência no value de $value");
+    print("Saldo da conta atualizado: $saldo");
+    print("");
+  }
+
+  void send(double value) {
+    saldo -= value;
+    print("$nome fez uma transferência no valor de $value");
+    print("Saldo da conta atualizado: $saldo");
+    print("");
+  }
+}
+
 void main() {
   print("Sistema de Contas Bancárias - POO em Dart");
   print("");
@@ -14,6 +38,8 @@ void main() {
   contaPaulo.saldo = 2500;
   print(contaPaulo.saldo);
 
+  contaPaulo.receiving(500);
+  contaPaulo.send(200);
   // Para exibir todas as contas que existem dentro da lista,
   // vamos utilizar um for in para percorrer a conta in Contas
 
@@ -23,12 +49,6 @@ void main() {
   for (Conta conta in contas) {
     print("Titular da conta: ${conta.nome}");
     print("${conta.nome}, saldo: ${conta.saldo}");
+    print("");
   }
-}
-
-class Conta {
-  String nome;
-  double saldo;
-
-  Conta(this.nome, this.saldo);
 }
